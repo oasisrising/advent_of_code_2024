8aa5bb0c084a2bc22fc6a4f6faca42ee84dccbb4
michele <michele.pease@coforma.io>
2024-12-02 10:53:14 -0600

    remove input

---
+++ day02/day02.py
@@ -0,0 +1,17 @@
+# Load data
+file = open('day02/test_input.txt').read();
+#file  = open('day02/input.txt').read();
+#print(file);
+
+lines = file.splitlines();
+#print(lines);
+
+list1 = [];
+list2 = [];
+# Prepare data
+for line in lines:
+    split = line.split();
+    print(split);
+
+#print(list1);
+#print(list2);
